ba9514c3a0b09c49e1727facbbf3c36501334381
BCarroll-GitHub <164546595+BCarroll-GitHub@users.noreply.github.com>
2024-03-29 15:59:08 -0600

    Delete experiments/vocals2008 directory

--- experiments/vocals2008/wband/osu/src/Pinsky/jtext.m
+++
@@ -1,9 +0,0 @@
-function h=jtext(str,x,y,varargin);
-  if ~exist('x','var')
-    x=.05;
-  end
-  if ~exist('y','var')
-    y=1.05;
-  end
-  h=text(x,y,str,'units','normalized','fontweight','bold',varargin{:});
-  end
